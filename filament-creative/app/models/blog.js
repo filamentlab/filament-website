86691f22d5fceeb5b90ad92c81b26e2eeb7afaae
Taha Jalil <taha@filamentlab.com>
2015-10-18 12:49:35 -0400

    created blog and single-blog posts templates and routes

--- filament-creative/app/models/blog.js
+++ filament-creative/app/models/blog.js
@@ -1,7 +1,29 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+DS.Model.extend({
   title: DS.attr('string'),
+  punchCopy: DS.attr('string'),
   author: DS.attr('string'),
   date: DS.attr('date')
 });
+
+// Blog.reopenClass({
+//     FIXTURES: [
+//         {
+//             id: 1,
+//             title: "First Blog Title",
+//             author: "Lindsay Trevors",
+//             punchCopy: "I am writting a blog",
+//             date: "05/25/2105"
+//         },
+//         {
+//             id: 2,
+//             title: "Second Blog Title",
+//             author: "Trevor Davson",
+//             punchCopy: "I am writting a blog",
+//             publishDate: "05/22/2105"
+//         }
+//     ]
+// });
+
+export default Blog;
