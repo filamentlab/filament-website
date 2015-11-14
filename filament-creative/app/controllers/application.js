45ffe764891be7a319ed8153243e7cc42cf1f48c
Taha Jalil <taha@filamentlab.com>
2015-11-14 14:47:15 -0500

    implemented campaigm monitor form, completed desktop style of contact form

--- filament-creative/app/controllers/application.js
+++ filament-creative/app/controllers/application.js
@@ -1,12 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    contactOpen: true,
+
     actions: {
         show_get_in_touch: function() {
             // later, change this so that on a click, liquid-container 
             // gets a class attached to it that makes it shrink/scale down
             // to the bottom
             Ember.$("#get-in-touch").toggle("slow");
-        }
+            this.toggleProperty('contactOpen');
+        },
+
+        input_field_in_focus: function() {
+            Ember.$('input:focus').siblings().css({
+                "left": "0",
+                "bottom": "0"
+            });
+        },
+
+        select_field_in_focus: function() {
+            Ember.$('select:focus').siblings().css({
+                "left": "0",
+                "bottom": "0"
+            });
+        },
+
+        textarea_in_focus: function() {
+            Ember.$('textarea:focus').siblings().css({
+                "left": "0",
+                "bottom": "0"
+            });
+        },
     }
 });
