83e71fed8a2aaeff4be66a29e41d5d60ddb6cdf8
Taha Jalil <taha.jalil@gmail.com>
2015-11-15 15:29:23 -0500

    completed mobile contact form view

--- filament-creative/app/controllers/application.js
+++ filament-creative/app/controllers/application.js
@@ -2,12 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     contactOpen: true,
+    // fieldInFocus: false,
 
     actions: {
         show_get_in_touch: function() {
             Ember.$(".liquid-container").addClass('minimize');
             Ember.$("#get-in-touch").addClass('active');
-            Ember.$('body').css("overflow", "hidden");
+            // Ember.$('body').css("overflow", "hidden");
             this.toggleProperty('contactOpen');
         },
 
@@ -21,22 +22,25 @@ export default Ember.Controller.extend({
         input_field_in_focus: function() {
             Ember.$('input:focus').siblings().css({
                 "left": "0",
-                "bottom": "0"
+                "bottom": "5px"
             });
+            // this.toggleProperty('fieldInFocus');
         },
 
         select_field_in_focus: function() {
             Ember.$('select:focus').siblings().css({
                 "left": "0",
-                "bottom": "0"
+                "bottom": "5px"
             });
+            // this.toggleProperty('fieldInFocus');
         },
 
         textarea_in_focus: function() {
             Ember.$('textarea:focus').siblings().css({
                 "left": "0",
-                "bottom": "0"
+                "bottom": "5px"
             });
+            // this.toggleProperty('fieldInFocus');
         },
     }
 });
