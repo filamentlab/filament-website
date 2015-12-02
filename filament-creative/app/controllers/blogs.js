1dccee193eb5f047baa0f4a19abc91585ab29922
Taha Jalil <taha@filamentlab.com>
2015-12-01 20:17:18 -0500

    homepage: added background animation to nav-prev and nav-next

--- filament-creative/app/controllers/blogs.js
+++ filament-creative/app/controllers/blogs.js
@@ -4,9 +4,9 @@ export default Ember.Controller.extend({
 	actions: {
 		changeImage: function(){
 			Ember.$('.blog-list li').on("mouseenter", function(){
-				var current = $(this).attr('class');
-				$('#image-array li').stop(true, false).fadeOut(500);
-				$('#image-array li.' + current).stop(true, false).fadeIn(500);
+				var current = Ember.$(this).attr('class');
+				Ember.$('#image-array li').stop(true, false).fadeOut(500);
+				Ember.$('#image-array li.' + current).stop(true, false).fadeIn(500);
 			});
 		},
 	}
